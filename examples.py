a92485faf42714e6501b75cfef820b42b480fef0
samajayarathna <55673667+samajayarathna@users.noreply.github.com>
2022-02-01 21:14:23 +1100

    without encryption

--- examples.py
+++ examples.py
@@ -1,74 +1,40 @@
-import io
-
-
-
-
-        
-# with open('credentials.txt','r') as fp:
-#    line = fp.readline()
-#    cnt = 1
-#    for line in fp:
-#        #print("User name {}: {}".format(cnt, line.strip()))
-#        print("User name {}: {}".format(cnt ,line.strip()))
-#        line = fp.readline()
-       
-import io
-
 
 #display Welcome to Password manager when program starts
 print("\nWelcome to Password manager\n")
 
+choice = 0
 
 def add_data():
-    
     #create a file called credentials.txt if it doesnt exist
-
     myfile =open("credentials.txt ", "a")
     userName = input("Please enter User name ")
-    charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`!@#$%^&*()_-=|\"':;?/>.<, "
-    encText = " ".join([charset[(charset.find(c)+3)%95] for c in userName])
-
-    myfile.seek(0)
-    data = myfile.read(100)
-    if len(data) >0 :
-        myfile.write("\n")
-        myfile.write(encText)
-
+    myfile.write("\n")
+    myfile.write(userName)
     # add password to the file
     password = input("Enter password ")
-    charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`!@#$%^&*()_-=|\"':;?/>.<, "
-    encText = " ".join([charset[(charset.find(c)+3)%95] for c in password])
-    myfile.write(encText)
-        
+    myfile.write(password)
     # add URL/Source to the file
     url = input("Enter URL/Source ")
-    myfile.write(url)
-
-       
+    myfile.write(url) 
     #close the file
     myfile.close()
-
     # display message 
     print("\nSuccessfully added your credentials\n\n")
 
 def display_data():
-    
-    print(f"{'User Name' :28} {'Password' : ^28} {'URL/Source' : >28}")
     f =open('credentials.txt','r')
-    lines = [line.strip() for line in f]
-    print(lines)
+    Lines = f.readlines()
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        print("User {}: {}".format(count, line.strip()))
         
-    #print(f"{num1 : <28} {num2 : ^28} {num3 : >28}")
-
-choice = 0
 while choice != 'q':
     print("\nType 1 to add credentials\n")
     print("Type 2 to View credentials\n")
     print("Type 'q' to Exit ")
-
     choice = input("\nMake your choice ")
-    
-    
     if choice == '1':
         add_data()
     elif choice == '2':
