813f7e34c905e54aa0771c140b53ec041b027a57
samajayarathna <55673667+samajayarathna@users.noreply.github.com>
2022-02-02 11:57:10 +1100

    to be reviewed

--- password_manager_new.py
+++ password_manager_new.py
@@ -1,5 +1,4 @@
 
-
 #display Welcome to Password manager when program starts
 print("\nWelcome to Password manager\n")
 
@@ -12,7 +11,7 @@ def add_data():
 
     # add username to the file
     userName = input("Please enter User name ")
-    myfile.write("\n")
+    #myfile.write("\n")
     myfile.write(userName)
     
     # add password to the file
@@ -24,7 +23,9 @@ def add_data():
     url = input("Enter URL/Source ")
     myfile.write(' ')
     myfile.write(url) 
+    myfile.write(' ')
    
+
     #close the file
     myfile.close()
 
@@ -36,18 +37,22 @@ def display_data():
     f =open('credentials.txt','r')
 
     #display data in command prompt
-    line_list = f.readlines()
-    items = []
-    for x in line_list:
-        items.append(x)
-    print(items)
+    data = f.read()
+    f.close()
+    dataList = data.split()
+    
+    print(f"{'User Name' :10} {'Password' : ^20} {'URL/Source' : >10}")
+    
+    i = 0
+    while i < len(dataList):
+        print(f"{dataList[i] :10} {dataList[i+1] : ^20} {dataList[i+2] : >10}")
+        i = i + 3
 
 #display choices to the user
 while choice != 'q':
     print("\nType 1 to add credentials\n")
     print("Type 2 to View credentials\n")
     print("Type 'q' to Exit ")
-
     #ask choice from the user
     choice = input("\nMake your choice ")
     if choice == '1':
@@ -60,4 +65,3 @@ while choice != 'q':
         break 
     else:
         print("invalid option. Please try again")
-
